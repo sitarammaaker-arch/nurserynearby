a229a197e57b05c9309d22e6ddec19cb01cc04ae
sitarammaaker-arch <sitaram.maaker@gmail.com>
2026-03-14 23:02:57 +0530

    Fix breadcrumbs build error

--- src/app/listing/[slug]/page.tsx
+++ src/app/listing/[slug]/page.tsx
@@ -100,6 +100,17 @@ export default async function ListingPage({ params }: Props) {
     }),
   };
 
+  const breadcrumbs = {
+    "@context": "https://schema.org",
+    "@type": "BreadcrumbList",
+    itemListElement: [
+      { "@type": "ListItem", position: 1, name: "Home",            item: SITE.url },
+      { "@type": "ListItem", position: 2, name: "Nursery",         item: `${SITE.url}/nursery/all` },
+      { "@type": "ListItem", position: 3, name: nursery.city.name, item: `${SITE.url}/nursery/${nursery.city.slug}` },
+      { "@type": "ListItem", position: 4, name: nursery.name,      item: `${SITE.url}/listing/${nursery.slug}` },
+    ],
+  };
+
   return (
     <>
       <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }} />
